a6115ed2da2a0fa37ed8bdc48b67df074c4ce66a
ScanAlign <atifjan2019@gmail.com>
2026-03-08 19:14:12 +0500

    Fix hydration warning from browser extensions

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" dir="ltr">
-            <body className={`${inter.variable} ${amiri.variable}`}>{children}</body>
+            <body className={`${inter.variable} ${amiri.variable}`} suppressHydrationWarning>{children}</body>
         </html>
     );
 }
